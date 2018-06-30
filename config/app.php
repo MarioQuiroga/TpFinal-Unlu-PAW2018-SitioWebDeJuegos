ca1fb313336418d2a1089b234ec64b4c3eb60bfc
MarioQuiroga <marioquiro17@gmail.com>
2018-06-30 18:55:44 -0300

    update app.php

--- config/app.php
+++ config/app.php
@@ -209,6 +209,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+       
 
 ]];
